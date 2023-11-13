37625b64477f7bcedc5b86ac23e7741a6179ef82
Lina María <66230572+linamaria126@users.noreply.github.com>
2023-11-13 13:57:17 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let subject = ["My brother", "My mother", "My dog", "My car"];
+  let action = ["fell", "got lost", "fell asleep", "it broke down"];
+  let where = [
+    "on the stairs",
+    "on the rooftop",
+    "in the Avenue",
+    "in the north"
+  ];
+
+  let excuse = "";
+
+  const subjectAleatorio = Math.floor(Math.random() * subject.length);
+  const actionAleatorio = Math.floor(Math.random() * action.length);
+  const whereAleatorio = Math.floor(Math.random() * where.length);
+
+  excuse =
+    subject[subjectAleatorio] +
+    " " +
+    action[actionAleatorio] +
+    " " +
+    where[whereAleatorio];
+  console.log(excuse);
+
+  return excuse;
 };
